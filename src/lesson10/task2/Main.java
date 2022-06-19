5d6d60bc233c68a943873224f7163b0af638d2c1
idel <idel_11@me.com>
2022-06-19 19:15:28 +0300

    Lesson10, tasks 2 implementation

--- src/lesson10/task2/Main.java
+++ src/lesson10/task2/Main.java
@@ -5,9 +5,23 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         MyList list = new MyList();
-        list.addElement(1);
-        list.addElement("test");
 
+        System.out.println("============ Перевірка метода addElement та toString ============");
+        list.addElement(0);
+        list.addElement("test_one");
         System.out.println(list);
+
+        System.out.println("============ Перевірка метода getByIndex ============");
+        System.out.println(list.getByIndex(0));
+        System.out.println(list.getByIndex(1));
+
+        System.out.println("============ Перевірка динамічного розширення масиву ============");
+        for (int i = 2; i < 10; i++) {
+            list.addElement("test" + i);
+            System.out.println(list);
+        }
+
+        System.out.println("============ Перевірка метода size ============");
+        System.out.println(list.size());
     }
 }
--- src/lesson10/task2/MyList.java
+++ src/lesson10/task2/MyList.java
@@ -1,17 +1,18 @@
 package lesson10.task2;
 
 
+import java.util.Arrays;
 import java.util.List;
 
-public class MyList <T> {
+public class MyList<T> {
 
-    private final int DEF_SIZE = 10;
-    private final int GROW = 3;
+    private final int DEF_SIZE = 5;
+    private final int GROW = 2;
     private int index = 0;
     Object[] array = new Object[DEF_SIZE];
 
-    public void addElement(T temp){
-        if (index == array.length - 1){
+    public void addElement(T temp) {
+        if (index == array.length - 1) {
             Object[] newArray = new Object[array.length + GROW];
             System.arraycopy(array, 0, newArray, 0, index);
             array = newArray;
@@ -19,4 +20,16 @@ public class MyList <T> {
         array[index++] = temp;
     }
 
+    public T getByIndex(int index) {
+        return (T) array[index];
+    }
+
+    public int size() {
+        return index;
+    }
+
+    @Override
+    public String toString() {
+        return "MyList{" + Arrays.toString(array) + "}";
+    }
 }
